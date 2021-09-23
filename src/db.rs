99a83070125825f8e50eeff1238eecd026e62887
Lyon <slimystudios@protonmail.com>
2021-09-23 12:44:36 +0200

    Completed database

--- src/db.rs
+++ src/db.rs
@@ -1,28 +1,44 @@
 use std::collections::{HashMap, HashSet};
+use std::iter::FromIterator;
+use itertools::chain;
+use phf::{phf_set};
 
-// TODO: Just copy strings
-pub const IGNORE: HashSet<&str> = HashSet::from([""]);
-pub const PRINT: HashSet<&str> = HashSet::from(["print", "imprimir", "imprimeix"]);
-pub const TRUE: HashSet<&str> = HashSet::from(["true", "cert", "veritat"]);
-pub const FALSE: HashSet<&str> = HashSet::from(["false", "fals", "mentida"]);
-pub const KEYWORD: fn() -> HashSet<&&'static str> = || {
-    let mut res: HashSet<&&str> = HashSet::new();
-    let keywords = vec![&IGNORE, &PRINT, &TRUE, &FALSE];
-    
-    for set in keywords {
-        for keyword in set {
-            res.insert(keyword);
+pub struct DB {
+    pub ignore: HashSet<&'static str>,
+    pub print: HashSet<&'static str>,
+    pub r#true: HashSet<&'static str>,
+    pub r#false: HashSet<&'static str>,
+    pub keyword: HashSet<&'static str>,
+}
+
+impl DB {
+    pub fn new(&self) -> Self {
+        DB {
+             ignore: HashSet::from(["a"]),
+              print: HashSet::from(["print", "imprimir", "imprimeix"]),
+               r#true: HashSet::from(["true", "cert", "veritat"]),
+              r#false: HashSet::from(["false", "fals", "mentida"]),
+            keyword: HashSet::from_iter(chain!(self.print, self.r#true, self.r#false)),
         }
     }
-    
-    println!("{:?}", res);
-    res
-};
 
-pub fn IS_LITERAL(string: &str) -> bool {
-           string.starts_with('"') 
-        || string.starts_with('\'') 
-        || string.parse::<i64>().is_ok() 
-        || TRUE.contains(string) 
-        || FALSE.contains(string)
+    pub fn is_literal(&self, string: &str) -> bool {
+        string.starts_with('"')
+            || string.starts_with('\'')
+            || string.parse::<i64>().is_ok()
+            || self.r#true.contains(string)
+            || self.r#false.contains(string)
+    }
 }
+
+
+
+/*
+const fn clone_sets<'a>(sets: &[HashSet<&'a str>]) -> HashSet<&'a str> {
+    let mut res : HashSet<&str> = HashSet::new();
+    
+    for set in sets {
+        res.extend(set);
+    }
+    res
+} */
