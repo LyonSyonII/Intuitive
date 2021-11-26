52a1d210c414610bca5748f098671951728080f7
LyonSyonII <slimystudios@protonmail.com>
2021-11-26 16:13:23 +0100

    Print now is able to print multiple variables

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
 mod parser;
 
-use std::env;
-use std::path::Path;
-use std::io::Write;
-use die::Die;
 use die::die;
+use die::Die;
+use std::env;
+use std::io::Write;
+use std::path::Path;
 
 fn main() {
     let mut args = env::args().skip(1);
-    if args.len() == 0 { die!("Usage: chat INPUT OUTPUT"); }
-    let inp = args.next().die("Error: Please, provide a valid input file name");
-    let out = args.next().die("Error: Pleas, provide an output file name");
-    let mut inp = std::fs::File::open(Path::new(&inp)).die(&format!("File with name '{}' not found", inp));
-    let mut out = std::fs::File::create(Path::new(&out)).die(&format!("File with name '{}' cannot be created", out));
-    out.write_all(parser::parse_file(&mut inp).as_bytes()).die("Could not write to file");
+    if args.len() == 0 {
+        die!("Usage: chat INPUT OUTPUT");
+    }
+    let inp = args
+        .next()
+        .die("Error: Please, provide a valid input file name");
+    let out_s = args
+        .next()
+        .die("Error: Please, provide an output file name");
+    let mut inp =
+        std::fs::File::open(Path::new(&inp)).die(&format!("File with name '{}' not found", inp));
+    let mut out = std::fs::File::create(Path::new(&(out_s.clone() + ".rs")))
+        .die(&format!("File with name '{}' cannot be created", out_s));
+    out.write_all(parser::parse_file(&mut inp).as_bytes())
+        .die("Could not write to file");
+    std::process::Command::new("rustc").args([out_s.clone() + ".rs", "-o".into(), out_s.clone()]).status().die("Looks like you don't have Rust installed, go to https://github.com/LyonSyonII/Intuitive and follow the installation instructions");
+    std::process::Command::new(format!("./{}", out_s))
+        .status()
+        .die("File cannot be executed");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,85 +1,129 @@
-use std::io::*;
-use std::fs::File;
 use die::Die;
-use pest::{ Parser as P, iterators::Pair, iterators::Pairs };
+use pest::{iterators::Pair, iterators::Pairs, Parser as P};
+use std::fs::File;
+use std::io::*;
 
 #[derive(pest_derive::Parser)]
 #[grammar = "grammar.pest"]
 struct Parser;
+
+#[derive(Default)]
+struct Global {
+    variables: std::collections::HashSet<String>,
+}
 
 pub fn parse_file(file: &mut File) -> String {
     // Read file into string
     let mut inp = String::new();
     file.read_to_string(&mut inp).die("Unable to read the file");
     let parse = Parser::parse(Rule::MAIN, &inp).die("Error parsing the file");
-    
-    let mut out: String = "fn main() {".into();
+
+    let mut out: String =
+        "#[allow(non_snake_case)]\n#[allow(unused_variables)]\n#[allow(unused_mut)]\nfn main() {"
+            .into();
+
+    let mut global = Global::default();
+
     // Read instructions
     for expr in parse {
-        out += &parse_expr(expr)
+        out += &parse_expr(expr, &mut global)
     }
     rustfmt_wrapper::rustfmt(out + "}").die("Could not format the input")
 }
-
-fn parse_expr(expr: Pair<Rule>) -> String {
+fn parse_expr(expr: Pair<Rule>, global: &mut Global) -> String {
     match expr.as_rule() {
-        Rule::Assign => parse_assig(expr.into_inner()),
+        Rule::Newline => "\n".into(),
+        Rule::Comment => parse_comment(expr.as_str()),
+        Rule::Def => parse_def(expr.into_inner(), global),
+        Rule::Assign => parse_assig(expr.into_inner(), global),
         Rule::Print => parse_print(expr.into_inner()),
-        _ => { String::new() }
+        _ => String::new(),
     }
 }
 
-fn parse_assig(mut pairs: Pairs<Rule>) -> String {
+fn parse_comment(comment: &str) -> String {
+    format!("/* {} */", comment)
+}
+
+fn parse_def(mut pairs: Pairs<Rule>, global: &mut Global) -> String {
     let name = pairs.next().unwrap().as_str();
     let rhs = pairs.next().unwrap();
-    let rhs: String = 
-        match rhs.as_rule() {
-            Rule::Name | Rule::Int | Rule::Float | Rule::String => rhs.as_str().into(),
-            Rule::Op => parse_op(rhs.into_inner()).into(),
-            _ => "".into(),
-        };
-    
-    format!("let {} = {};", name, rhs)
+    let rhs: String = match rhs.as_rule() {
+        Rule::Name | Rule::Int | Rule::Float | Rule::String => rhs.as_str().into(),
+        Rule::Op => parse_op(rhs.into_inner()).into(),
+        _ => "".into(),
+    };
+
+    global.variables.insert(name.into());
+    match rhs.is_empty() {
+        true => format!("let mut {};", name),
+        false => format!("let mut {} = {};", name, rhs),
+    }
+}
+
+fn parse_assig(mut pairs: Pairs<Rule>, global: &mut Global) -> String {
+    let name = pairs.next().unwrap().as_str();
+    let rhs = pairs.next().unwrap();
+    let rhs: String = match rhs.as_rule() {
+        Rule::Name | Rule::Int | Rule::Float | Rule::String => rhs.as_str().into(),
+        Rule::Op => parse_op(rhs.into_inner()).into(),
+        _ => "".into(),
+    };
+
+    match global.variables.contains(name) {
+        true => format!("{} = {};", name, rhs),
+        false => {
+            global.variables.insert(name.into());
+            format!("let mut {} = {};", name, rhs)
+        }
+    }
 }
 
 fn parse_op(mut pairs: Pairs<Rule>) -> String {
     let pairs = pairs.next().unwrap();
-    let sym = 
-        match pairs.as_rule() {
-            Rule::Add => '+',
-            Rule::Sub => '-',
-            Rule::Mul => '*',
-            Rule::Div => '/',
-            _ => '\0',
-        };
-    
+    let sym = match pairs.as_rule() {
+        Rule::Add => '+',
+        Rule::Sub => '-',
+        Rule::Mul => '*',
+        Rule::Div => '/',
+        _ => '\0',
+    };
+
     let mut pairs = pairs.into_inner();
     let lhs = pairs.next().unwrap();
     let rhs = pairs.next().unwrap();
-    
-    let lhs = 
-        if lhs.as_rule() == Rule::Op {
-            // Return underlying operation
-            parse_op(lhs.into_inner())
-        }
-        else {
-            // Return Value itself
-            lhs.as_str().into()
-        };
-    
-    let rhs = 
-        if rhs.as_rule() == Rule::Op {
-            // Return underlying operation
-            parse_op(rhs.into_inner())
-        }
-        else {
-            // Return Value itself
-            rhs.as_str().into()
-        };
+
+    let lhs = if lhs.as_rule() == Rule::Op {
+        // Return underlying operation
+        parse_op(lhs.into_inner())
+    } else {
+        // Return Value itself
+        lhs.as_str().into()
+    };
+
+    let rhs = if rhs.as_rule() == Rule::Op {
+        // Return underlying operation
+        parse_op(rhs.into_inner())
+    } else {
+        // Return Value itself
+        rhs.as_str().into()
+    };
 
     format!("{} {} {}", lhs, sym, rhs)
 }
 
-fn parse_print(mut pairs: Pairs<Rule>) -> String {
-    
+fn parse_print(pairs: Pairs<Rule>) -> String {
+    let mut res = String::from("println!(\"");
+    let mut rhs = String::new();
+    for pair in pairs {
+        res += "{}";
+        rhs += &(", ".to_owned()
+            + &match pair.as_rule() {
+                Rule::Name | Rule::Int | Rule::Float | Rule::String => pair.as_str().into(),
+                Rule::Op => parse_op(pair.into_inner()),
+                _ => "".into(),
+            });
+    }
+
+    format!("{}\"{});", res, rhs)
 }
