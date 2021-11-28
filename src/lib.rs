50e16ae5420d6050b2e46c838eca68a681086c65
LyonSyonII <slimystudios@protonmail.com>
2021-11-28 00:04:13 +0000

    Added if logic. Added resulting operation type, it defaults to float. Int types do not change, only result. (As ints are floats for Rust).

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,89 @@ mod parser;
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn basic_parsing() {
-		assert_eq!(parser::parse_string("\n"	), "\n"		);
-		assert_eq!(parser::parse_string("\n\n"	), "\n\n"	);
-		assert_eq!(parser::parse_string(" "		), ""		);
-		assert_eq!(parser::parse_string("   "	), ""		);
-		assert_eq!(parser::parse_string("is to" ), ""		);
-	}
-	
-	#[test]
-	fn assign() {
-		assert_eq!(parser::parse_string("A is equal to 10."), "let mut A = 10.0;");
-		assert_eq!(parser::parse_string("A equal 1."), "let mut A = 1.0;");
-		assert_eq!(parser::parse_string("A = 999."), "let mut A = 999.0;");
-		
-		assert_eq!(parser::parse_string("A is equal to 10,5."), "let mut A = 10.5;");
-		assert_eq!(parser::parse_string("A equal 15,1."), "let mut A = 15.1;");
-		assert_eq!(parser::parse_string("A = 0,56."), "let mut A = 0.56;");
-		
-		assert_eq!(parser::parse_string("A is equal to \"Hello\"."), "let mut A = \"Hello\";");
-		assert_eq!(parser::parse_string("A equal \"Hello\"."), "let mut A = \"Hello\";");
-		assert_eq!(parser::parse_string("A = \"Hello\"."), "let mut A = \"Hello\";");
-		
-		assert_eq!(parser::parse_string("A = 10.\nB = A."), "let mut A = 10.0;\nlet mut B = A;");
-	}
+    fn assert_parse(inp: &str, out: &str) {
+        assert_eq!(parser::parse_string(inp), out);
+    }
+
+    #[test]
+    fn basic_parsing() {
+        assert_parse("\n", "\n");
+        assert_parse("\n\n", "\n\n");
+        assert_parse(" ", "");
+        assert_parse("   ", "");
+        assert_parse("is to", "");
+    }
+
+    #[test]
+    fn assign() {
+        assert_parse("A is equal to 10.", "let mut A = 10.0;");
+        assert_parse("A equal 1.", "let mut A = 1.0;");
+        assert_parse("A = 999.", "let mut A = 999.0;");
+
+        assert_parse("A is equal to 10,5.", "let mut A = 10.5;");
+        assert_parse("A equal 15,1.", "let mut A = 15.1;");
+        assert_parse("A = 0,56.", "let mut A = 0.56;");
+
+        assert_parse("A is equal to \"Hello\".", "let mut A = \"Hello\";");
+        assert_parse("A equal \"Hello\".", "let mut A = \"Hello\";");
+        assert_parse("A = \"Hello\".", "let mut A = \"Hello\";");
+
+        assert_parse("A = 10.\nB = A.", "let mut A = 10.0;\nlet mut B = A;");
+    }
+
+    fn gen_op_test(equals: &[&str], operations: &[&str], correct_eq: &str, correct_op: &str) {
+    	for eq in equals {
+    		for op in operations {
+    			for (x, y) in &[("5.0", "10.0"), ("3,1416", "1,0"), ("3.0", "25,125"), ("A", "1.0"), ("A", "2,5"), ("A", "A")] {
+    				let test = format!("A {} {} {} {}.", eq, x.replace(".0", ""), op, y.replace(".0", ""));
+    				let answer = format!("let mut A {} {} {} {};", correct_eq, x.replace(',', "."), correct_op, y.replace(',', "."));
+    				println!("Test: {}", test);
+    				println!("Answer: {}", &answer);
+    				assert_parse(&test, &answer);
+    			}
+    		}
+    	}
+    }
+
+    #[test]
+    fn add() {
+    	gen_op_test(
+    		&["is equal to", "equal", "es igual a", "="],
+    		&["plus", "mes", "+"],
+    		"=",
+    		"+",
+    	);
+    }
+
+    #[test]
+    fn sub() {
+         	gen_op_test(
+    		&["is equal to", "equal", "es igual a", "="],
+    		&["minus", "menys", "-"],
+    		"=",
+    		"-",
+    	);
+    }
+
+    #[test]
+    fn mul() {
+    	gen_op_test(
+    		&["is equal to", "equal", "es igual a", "="],
+    		&["times", "multiplied by", "per",  "multiplicat per", "*"],
+    		"=",
+    		"*",
+    	);
+    }
+
+    #[test]
+    fn div() {
+           	gen_op_test(
+    		&["is equal to", "equal", "es igual a", "="],
+    		&["divided by", "entre", "dividit entre"],
+    		"=",
+    		"/",
+    	);    
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use die::{Die, die};
+use die::{die, Die};
 use pest::{iterators::Pair, iterators::Pairs, Parser as P};
 use std::fs::File;
 use std::io::*;
@@ -7,9 +7,24 @@ use std::io::*;
 #[grammar = "grammar.pest"]
 struct Parser;
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct Global {
     variables: std::collections::HashMap<String, Rule>,
+}
+
+pub fn parse_string(inp: &str) -> String {
+    let parse = Parser::parse(Rule::MAIN, &inp);
+    let parse = match parse {
+        Ok(p) => p,
+        Err(e) => die!(e.to_string()),
+    };
+
+    let mut global = Global::default();
+    let mut out = String::new();
+    for expr in parse {
+        out += &parse_expr(expr, &mut global);
+    }
+    out
 }
 
 pub fn parse_file(file: &mut File) -> String {
@@ -28,31 +43,19 @@ pub fn parse_file(file: &mut File) -> String {
     for expr in parse {
         out += &parse_expr(expr, &mut global)
     }
-    rustfmt_wrapper::rustfmt(out + "}").die("Could not format the input")
+
+    rustfmt_wrapper::rustfmt(out + "}").die("ERROR: Rustfmt could not format the input")
 }
 
-pub fn parse_string(inp: &str) -> String {
-	let parse = Parser::parse(Rule::MAIN, &inp);
-	let parse = match parse {
-		Ok(p) => p,
-		Err(e) => die!(e.to_string()),
-	};
-
-	let mut global = Global::default();
-	let mut out = String::new();
-	for expr in parse {
-		out += &parse_expr(expr, &mut global);
-	}
-	out
-}
-
-fn parse_expr(expr: Pair<Rule>, global: &mut Global) -> String {
+fn parse_expr(expr: Pair<Rule>, mut global: &mut Global) -> String {
+	println!("{:?}", global);
     match expr.as_rule() {
         Rule::Newline => "\n".into(),
         Rule::Comment => parse_comment(expr.as_str()),
         Rule::Def => parse_def(expr.into_inner(), global),
-        Rule::Assign => parse_assig(expr.into_inner(), global),
+        Rule::Assign => parse_assig(expr.into_inner(), &mut global),
         Rule::Print => parse_print(expr.into_inner(), global),
+        Rule::If => parse_if(expr.into_inner(), &mut global),
         _ => String::new(),
     }
 }
@@ -61,13 +64,15 @@ fn parse_comment(comment: &str) -> String {
     format!("/* {} */", comment)
 }
 
+// TODO: Acabar de fer parse_def (falta afegir parse_op actualitzat)
 fn parse_def(mut pairs: Pairs<Rule>, global: &mut Global) -> String {
     let name = pairs.next().unwrap().as_str();
     let rhs = pairs.next().unwrap();
     let rule = rhs.as_rule();
     let rhs: String = match rule {
         Rule::Name | Rule::Int | Rule::Float | Rule::String => rhs.as_str().into(),
-        Rule::Op => parse_op(rhs.into_inner(), &global).into(),
+        Rule::FmtString => parse_fmt_string(rhs.into_inner()),
+        Rule::Op => parse_op(rhs.into_inner(), &global).0,    
         _ => "".into(),
     };
 
@@ -78,87 +83,110 @@ fn parse_def(mut pairs: Pairs<Rule>, global: &mut Global) -> String {
     }
 }
 
-fn parse_assig(mut pairs: Pairs<Rule>, global: &mut Global) -> String {
+fn parse_assig(mut pairs: Pairs<Rule>, mut global: &mut Global) -> String {
     let name = pairs.next().unwrap().as_str();
     let rhs = pairs.next().unwrap();
     let mut rule = rhs.as_rule();
     let rhs: String = match rule {
         Rule::String => rhs.as_str().into(),
+        Rule::FmtString => parse_fmt_string(rhs.into_inner()),
         Rule::Int => rhs.as_str().to_owned() + ".0",
         Rule::Float => rhs.as_str().replace(',', "."),
         Rule::Name => {
-        	let ret = rhs.as_str().into();
-        	rule = global.variables[ret];
-        	ret.into()
+            let ret = rhs.as_str().into();
+            rule = *global.variables.get(ret).unwrap_or(&Rule::Float);
+            ret.into()
         }
-        Rule::Op => parse_op(rhs.into_inner(), &global).into(),
+        Rule::Op => {
+        	let ret = parse_op(rhs.into_inner(), &global);
+        	rule = ret.1;
+        	ret.0
+        }
         _ => "".into(),
     };
-
-    match global.variables.contains_key(name) && rule == global.variables[name] {
+    	
+    match global.variables.contains_key(name) && (rule == global.variables[name] || rule == Rule::Float) {
         true => format!("{} = {};", name, rhs),
         false => {
-        	global.variables.insert(name.into(), rule);
+            global.variables.insert(name.into(), rule);
             format!("let mut {} = {};", name, rhs)
         }
     }
 }
 
-fn parse_op(mut pairs: Pairs<Rule>, global: &Global) -> String {
+fn parse_print(pairs: Pairs<Rule>, global: &Global) -> String {
+    let mut lhs = String::from("println!(\"");
+    let mut rhs = String::new();
+    for pair in pairs {
+        lhs += "{}";
+        let pair = match pair.as_rule() {
+            Rule::Name | Rule::Int | Rule::String => pair.as_str().into(),
+            Rule::Float => pair.as_str().replace(',', "."),
+            Rule::Op => parse_op(pair.into_inner(), &global).0,
+            _ => "".into(),
+        };
+    	rhs = format!("{}, {}", rhs, pair);
+    }
+
+    format!("{}\"{});", lhs, rhs)
+}
+
+fn parse_if(mut pairs: Pairs<Rule>, mut global: &mut Global) -> String {
+	let mut lhs = String::from("if ");
+	let cmp = parse_op(pairs.next().unwrap().into_inner(), &global).0 + "{";
+	lhs += &cmp;
+
+	for pair in pairs {
+		lhs += &parse_expr(pair, &mut global);
+	}
+
+	lhs + "}"
+}
+
+fn parse_op(mut pairs: Pairs<Rule>, global: &Global) -> (String, Rule) {
     let pairs = pairs.next().unwrap();
     let sym = match pairs.as_rule() {
-        Rule::Add => '+',
-        Rule::Sub => '-',
-        Rule::Mul => '*',
-        Rule::Div => '/',
-        _ => '\0',
+        Rule::Add => "+",
+        Rule::Sub => "-",
+        Rule::Mul => "*",
+        Rule::Div => "/",
+        Rule::Less => "<",
+        Rule::LessEq => "<=",
+        Rule::More => ">",
+        Rule::MoreEq => ">=",
+        Rule::EqCmp => "==",
+        _ => "",
     };
 
-	let parse_side = |hs: Pair<Rule>| -> String {
-    	match hs.as_rule() {
+// TODO: Afegir Rule::NonInitialized que es detecti quan Rule::Name fagi l'unwrap_or i que fagi error de compilacio
+    let parse_side = |hs: Pair<Rule>| -> (String, Rule) {
+        match hs.as_rule() {
     		Rule::Op => parse_op(hs.into_inner(), &global),
-    		Rule::Int /*if rrule == Rule::Float*/ => hs.as_str().to_owned() + ".0",
-    		Rule::Float => hs.as_str().replace(',', "."),
-    		_ => hs.as_str().into()
+    		Rule::Int => (hs.as_str().to_owned() + ".0", Rule::Int),
+    		Rule::Float => (hs.as_str().replace(',', "."), Rule::Float),
+			Rule::Name => {
+				let hs = hs.as_str().to_owned();
+				let rule = *global.variables.get(&hs).unwrap_or(&Rule::Float);
+				(hs, rule)
+			}
+    		_ => (hs.as_str().into(), Rule::WHITESPACE)
     	}
-	};
-    
+    };
+
     let mut pairs = pairs.into_inner();
     let lhs = parse_side(pairs.next().unwrap());
     let rhs = parse_side(pairs.next().unwrap());
-	/*
-    if lrule == Rule::Name && rrule == Rule::Name {
-    	let lrule = global.variables[&lhs];
-    	println!("Lhs rule: {:?}", lrule);
-    	let rrule = global.variables[&rhs];
-    	println!("Rhs rule: {:?}", rrule);
-    	
-    	if lrule != rrule {
-    		match lrule {
-    			Rule::Int => lhs = format!("({} as f32)", lhs),
-    			Rule::Float => rhs = format!("({} as f32)", rhs),
-    			_ => {},
-    		}
-    	}
-    }
-    */
-
-    format!("{} {} {}", lhs, sym, rhs)
+    let rule = if lhs.1 == rhs.1 { lhs.1 } else { Rule::Float };
+    (format!("{} {} {}", lhs.0, sym, rhs.0), rule)
 }
 
-fn parse_print(pairs: Pairs<Rule>, global: &Global) -> String {
-    let mut res = String::from("println!(\"");
-    let mut rhs = String::new();
-    for pair in pairs {
-        res += "{}";
-        rhs += &(", ".to_owned()
-            + &match pair.as_rule() {
-                Rule::Name | Rule::Int | Rule::String => pair.as_str().into(),
-                Rule::Float => pair.as_str().replace(',', ".").into(),
-                Rule::Op => parse_op(pair.into_inner(), &global),
-                _ => "".into(),
-            });
-    }
+fn parse_fmt_string(mut pairs: Pairs<Rule>) -> String {
+	let mut lhs = String::from("format!(\"");
+	let mut rhs = String::new();
+	for pair in pairs {
+		lhs += "{}";
+		rhs += &format!(", {}", pair.as_str());
+	}
 
-    format!("{}\"{});", res, rhs)
+	format!("{}\"{})", lhs, rhs)
 }
